da78819374d046de948fefc6b3c0e51c821a84d8
doctorai-in <54732569+doctorai-in@users.noreply.github.com>
2021-01-14 16:56:20 +0530

    Add files via upload

---
+++ start-training.sh
@@ -0,0 +1,3 @@
+python3 model_main_tf2.py --model_dir=models/my_ssd_resnet50_v1_fpn --pipeline_config_path=models/my_ssd_resnet50_v1_fpn/pipeline.config --checkpoint_dir=models/my_ssd_resnet50_v1_fpn
+python3 .\exporter_main_v2.py --input_type image_tensor --pipeline_config_path .\models\my_efficientdet_d1\pipeline.config --trained_checkpoint_dir .\models\my_efficientdet_d1\ --output_directory .\exported-models\my_model
+
